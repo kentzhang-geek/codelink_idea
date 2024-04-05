67d83253915f373426e77f8d0e064bd758a69dbb
GitHub Action <action@github.com>
2024-04-05 22:44:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/kentzhanggeek/codelinkidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kentzhanggeek/codelinkidea/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.kentzhanggeek.codelinkidea.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kentzhanggeek.codelinkidea.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
